c5635f80ffadcd69a10b4d1b825b332fa448757c
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-09-08 11:12:24 -0300

    added app.js initial code, converted to ESM

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,10 @@
-console.log("hey")
+import http from "http";
+import { app } from "./app.js";
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
