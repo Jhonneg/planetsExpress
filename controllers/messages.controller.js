4b9fe706550ad8c987ad51137a6f29771ffc64b6
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-09-08 10:40:02 -0300

    initial front end starter files

--- controllers/messages.controller.js
+++
@@ -1,3 +0,0 @@
-export function postMessages(req, res) {
-  console.log("Updating messages...");
-}
--- routes/messages.router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-import { postMessages } from "../controllers/messages.controller.js";
-
-export const messagesRounter = express.Router();
-
-messagesRounter.post("/", postMessages);
