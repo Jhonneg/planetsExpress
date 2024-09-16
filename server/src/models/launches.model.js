49a2fc73acf0da0e55046fb9e4be1c2544b5bb77
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-09-16 17:32:03 -0300

    mission launch function working

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
   mission: "Keplex Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
+  target: "Kepler-442 b",
   costumers: ["Bidu", "NASA"],
   upcoming: true,
   success: true,
