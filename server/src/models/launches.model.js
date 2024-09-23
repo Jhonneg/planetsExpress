0e576451d640bb0bdf8e39a14a0c905381ff4a19
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-09-22 21:43:28 -0300

    updated abort launch controller for mongoDB

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -2,7 +2,6 @@ const launchesDatabase = require("./launches.mongo");
 const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
-const launches = new Map();
 
 const launch = {
   flightNumber: 100,
@@ -16,8 +15,10 @@ const launch = {
 };
 saveLaunch(launch);
 
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+  return await launchesDatabase.findOne({
+    flightNumber: launchId,
+  });
 }
 
 async function getLatestFlightNumber() {
@@ -49,7 +50,7 @@ async function saveLaunch(launch) {
     throw new Error("No matching planet was found");
   }
 
-  await launchesDatabase.updateOne(
+  await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
     },
@@ -73,11 +74,18 @@ async function scheduleNewLaunch(launch) {
   await saveLaunch(newLaunch);
 }
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+async function abortLaunchById(launchId) {
+  const aborted = await launchesDatabase.updateOne(
+    {
+      flightNumber: launchId,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
+  );
+  
+  return aborted.modifiedCount === 1;
 }
 
 module.exports = {
