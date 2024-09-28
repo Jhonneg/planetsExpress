e267e645352d201786c418b327f7be21bd9ab79b
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-09-28 20:09:32 -0300

    refactored launches model code

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -19,7 +19,7 @@ saveLaunch(launch);
 
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
-async function loadLaunchData() {
+async function populateLaunches() {
   console.log("Downloading launch data...");
 
   const response = await axios.post(SPACEX_API_URL, {
@@ -40,7 +40,6 @@ async function loadLaunchData() {
           },
         },
       ],
-      
     },
   });
 
@@ -65,8 +64,25 @@ async function loadLaunchData() {
   }
 }
 
+async function loadLaunchData() {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: "Falcon 1",
+    mission: "FalconSat",
+  });
+  if (findLaunch) {
+    console.log("Launch data already loaded!");
+  } else {
+    await populateLaunches();
+  }
+}
+
+async function findLaunch(filter) {
+  return await launchesDatabase.find(filter);
+}
+
 async function existsLaunchWithId(launchId) {
-  return await launchesDatabase.findOne({
+  return await findLaunch({
     flightNumber: launchId,
   });
 }
