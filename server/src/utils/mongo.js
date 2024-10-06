9c4c332c8fa46d9b696850af6d5570e8e33a51f6
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-10-05 22:32:38 -0300

    added mongo to workflow

--- server/src/utils/mongo.js
+++ server/src/utils/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const MONGO_URL = process.env.DB_URL;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
